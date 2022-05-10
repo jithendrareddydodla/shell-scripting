437003843cc112b33de7ffd775853944e34bda26
dell <jithendrareddydodla@gmail.com>
2022-05-10 20:24:20 +0530

    for practices

--- roboshop-practice/components/frontend.sh
+++ roboshop-practice/components/frontend.sh
@@ -1,15 +1,11 @@
 #!/usr/bin/env bash
 
-user_id=$(id -u)
-if [ "$user_id" -ne 0]; then
-  echo you need to run this as your a root user
 
-else
 yum install nginx -y
 systemctl enable nginx
 systemctl start nginx
 curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
-cd /usr/share/nginx/html
+cd  /usr/share/nginx/html
 rm -rf *
 unzip /tmp/frontend.zip
 mv frontend-main/* .
@@ -17,4 +13,3 @@ mv static/* .
 rm -rf frontend-main README.md
 mv localhost.conf /etc/nginx/default.d/roboshop.conf
 systemctl restart nginx
-fi
