38ae9054f37f6329b4d6ec86fb090a14a7fa8581
dell <jithendrareddydodla@gmail.com>
2022-05-11 23:21:15 +0530

    for practices

--- roboshop-practice/components/frontend.sh
+++ roboshop-practice/components/frontend.sh
@@ -4,18 +4,18 @@ source components/common.sh
 
 CheckRootUser
 
-  yum install nginx -y
-  systemctl enable nginx
-  systemctl start nginx
-  curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
-  cd  /usr/share/nginx/html
+  yum install nginx -y >/tmp/roboshop_output.log
+  systemctl enable nginx >/tmp/roboshop_output.log
+  systemctl start nginx >/tmp/roboshop_output.log
+  curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" >/tmp/roboshop_output.log
+  cd  /usr/share/nginx/html >/tmp/roboshop_output.log
   rm -rf *
-  unzip /tmp/frontend.zip
-  mv frontend-main/* .
-  mv static/* .
+  unzip /tmp/frontend.zip >/tmp/roboshop_output.log
+  mv frontend-main/* . >/tmp/roboshop_output.log
+  mv static/* . >/tmp/roboshop_output.log
   rm -rf frontend-main README.md
-  mv localhost.conf /etc/nginx/default.d/roboshop.conf
-  systemctl restart nginx
+  mv localhost.conf /etc/nginx/default.d/roboshop.conf >/tmp/roboshop_output.log
+  systemctl restart nginx >/tmp/roboshop_output.log
 
 
 
