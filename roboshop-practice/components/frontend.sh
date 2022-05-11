a5c337be995e02c3dc1663307609877251cf49ab
dell <jithendrareddydodla@gmail.com>
2022-05-12 00:24:57 +0530

    for practices

--- roboshop-practice/components/frontend.sh
+++ roboshop-practice/components/frontend.sh
@@ -5,35 +5,35 @@ source components/common.sh
 CheckRootUser
 
   echo "INSTALLING NGINX"
-  yum install nginx -y >>${LOG_FILE}
+  yum install nginx -y >${LOG_FILE}
   Statuscheck
 
   echo "ENABLING NGINX"
-  systemctl enable nginx >>${LOG_FILE}
+  systemctl enable nginx >${LOG_FILE}
   Statuscheck
 
   echo "STARTING NGINX"
-  systemctl start nginx >>${LOG_FILE}
+  systemctl start nginx >${LOG_FILE}
   Statuscheck
 
   echo "LOADING FILE FROM GITHUB"
-  curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" >>${LOG_FILE}
+  curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" >${LOG_FILE}
   Statuscheck
 
-  cd  /usr/share/nginx/html >>${LOG_FILE}
+  cd  /usr/share/nginx/html >${LOG_FILE}
   rm -rf *
 
   echo "UNZIPING FILES"
-  unzip /tmp/frontend.zip >>${LOG_FILE}
+  unzip /tmp/frontend.zip >${LOG_FILE}
   Statuscheck
 
-  mv frontend-main/* . >>${LOG_FILE}
-  mv static/* . >>${LOG_FILE}
+  mv frontend-main/* . >${LOG_FILE}
+  mv static/* . >${LOG_FILE}
   rm -rf frontend-main README.md
-  mv localhost.conf /etc/nginx/default.d/roboshop.conf >>${LOG_FILE}
+  mv localhost.conf /etc/nginx/default.d/roboshop.conf >${LOG_FILE}
 
   echo "RESTARTING NGINX"
-  systemctl restart nginx >>${LOG_FILE}
+  systemctl restart nginx >${LOG_FILE}
   Statuscheck
 
 
