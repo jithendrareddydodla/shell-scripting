a51fc12fb2585e378f70de0f74783353ba0d13c4
dell <jithendrareddydodla@gmail.com>
2022-05-11 21:56:10 +0530

    for practices

--- roboshop-practice/components/frontend.sh
+++ roboshop-practice/components/frontend.sh
@@ -4,18 +4,18 @@ USER_ID=$(id -u)
 if [ "$USER_ID" -ne 0 ]; then
   echo You need to run this as ROOT USER
 else
-
-yum install nginx -y
-systemctl enable nginx
-systemctl start nginx
-curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
-cd  /usr/share/nginx/html
-rm -rf *
-unzip /tmp/frontend.zip
-mv frontend-main/* .
-mv static/* .
-rm -rf frontend-main README.md
-mv localhost.conf /etc/nginx/default.d/roboshop.conf
-systemctl restart nginx
+  yum install nginx -y
+  systemctl enable nginx
+  systemctl start nginx
+  curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
+  cd  /usr/share/nginx/html
+  rm -rf *
+  unzip /tmp/frontend.zip
+  mv frontend-main/* .
+  mv static/* .
+  rm -rf frontend-main README.md
+  mv localhost.conf /etc/nginx/default.d/roboshop.conf
+  systemctl restart nginx
 
 fi
+
