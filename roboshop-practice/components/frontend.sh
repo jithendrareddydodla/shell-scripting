05faa0165325a4d12291a87b68b3bc9ceb91a1e9
dell <jithendrareddydodla@gmail.com>
2022-05-11 23:41:36 +0530

    for practices

--- roboshop-practice/components/frontend.sh
+++ roboshop-practice/components/frontend.sh
@@ -15,18 +15,43 @@ fi
 
   echo "ENABLING NGINX"
   systemctl enable nginx >/tmp/roboshop_output.log
+  if [ $? -eq 0 ]; then
+    echo -e "\e[32m This stage is Sucess\e[0m"
+  else
+     echo -e "\e[31m This stage is Failure\e[0m"
+    exit 1
+  fi
 
   echo "STARTING NGINX"
   systemctl start nginx >/tmp/roboshop_output.log
+  if [ $? -eq 0 ]; then
+    echo -e "\e[32m This stage is Sucess\e[0m"
+  else
+     echo -e "\e[31m This stage is Failure\e[0m"
+    exit 1
+  fi
 
   echo "LOADING FILE FROM GITHUB"
   curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" >/tmp/roboshop_output.log
+if [ $? -eq 0 ]; then
+  echo -e "\e[32m This stage is Sucess\e[0m"
+else
+   echo -e "\e[31m This stage is Failure\e[0m"
+  exit 1
+fi
 
   cd  /usr/share/nginx/html >/tmp/roboshop_output.log
   rm -rf *
 
   echo "UNZIPING FILES"
   unzip /tmp/frontend.zip >/tmp/roboshop_output.log
+  if [ $? -eq 0 ]; then
+    echo -e "\e[32m This stage is Sucess\e[0m"
+  else
+     echo -e "\e[31m This stage is Failure\e[0m"
+    exit 1
+  fi
+
   mv frontend-main/* . >/tmp/roboshop_output.log
   mv static/* . >/tmp/roboshop_output.log
   rm -rf frontend-main README.md
@@ -34,6 +59,12 @@ fi
 
   echo "RESTARTING NGINX"
   systemctl restart nginx >/tmp/roboshop_output.log
+  if [ $? -eq 0 ]; then
+    echo -e "\e[32m This stage is Sucess\e[0m"
+  else
+     echo -e "\e[31m This stage is Failure\e[0m"
+    exit 1
+  fi
 
 
 
