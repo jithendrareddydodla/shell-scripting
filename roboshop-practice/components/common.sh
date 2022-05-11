256d0b96ab334a902e1b5ec0f14e6e78290021e0
dell <jithendrareddydodla@gmail.com>
2022-05-11 22:50:59 +0530

    for practices

--- roboshop-practice/components/common.sh
+++ roboshop-practice/components/common.sh
@@ -3,8 +3,8 @@ CheckRootUser() {
 
   if [ "$USER_ID" -ne "0" ]; then
     echo -e "\e[31mYou need to run this as root user\e[om"
-    exit 4
+    exit 1
   else
     echo -e "\e[32mok\e[0m"
-    fi
+  fi
 }
