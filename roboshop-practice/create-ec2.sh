3111fc3a6e6edcfbd6ed4fa0fd50d55cb1ecab29
dell <jithendrareddydodla@gmail.com>
2022-05-14 12:31:12 +0530

    for practices

--- roboshop-practice/create-ec2.sh
+++ roboshop-practice/create-ec2.sh
@@ -1,3 +1,13 @@
 #!/usr/bin/env bash
 
-aws ec2 run-instances --image-id ami-0bb6af715826253bf --instance-type t2.micro --instance-market-options "MarketType=spot,SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}" --tag-specifications "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=Frontend}]" "ResourceType=instance,Tags=[{Key=Name,Value=Frontend}]"
+if [ -z "$1" ]; then
+  echo You need to provide Instance name as argument
+  exit 1
+fi
+
+Name=$1
+
+
+AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=Centos-7-DevOps-Practice" --output table | grep ImageId | awk '{print $4}')
+
+aws ec2 run-instances --image-id ${AMI_ID} --instance-type t2.micro --instance-market-options "MarketType=spot,SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}" --tag-specifications "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=${Name}]" "ResourceType=instance,Tags=[{Key=Name,Value=${Name}]"
