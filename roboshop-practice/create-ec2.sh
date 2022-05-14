64b8635b99bd31a5cae13e47f97f9d5005d5644a
dell <jithendrareddydodla@gmail.com>
2022-05-14 19:52:44 +0530

    for practices

--- roboshop-practice/create-ec2.sh
+++ roboshop-practice/create-ec2.sh
@@ -16,3 +16,22 @@ fi
 AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=Centos-7-DevOps-Practice" --output table | grep ImageId | awk '{print $4}')
 
 aws ec2 run-instances --image-id ${AMI_ID} --instance-type t2.micro --instance-market-options "MarketType=spot,SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}" --tag-specifications "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=${NAME}}]" "ResourceType=instance,Tags=[{Key=Name,Value=${NAME}}]" &>/dev/null
+echo Instance Created
+
+sleep 30
+
+INSTANCE_ID=$(aws ec2 describe-spot-instance-requests --filters Name=tag:Name,Values=cart Name=state,Values=active --output table | grep InstanceId | awk '{print $4}')
+
+IPADDRESS= $(aws ec2 describe-instance --instance-ids ${INSTANCE_ID} --output table | grep PrivateIpAddress | head -n 1 | awk '{print $4}')
+
+sed -e "s/component/${NAME}/" -e "s/IPADDRESS/${IPADDRESS}/" record.json >/tmp/record.json
+aws route53 change-resourse-record-sets --hosted-zone-id Z0251361SXB48AR1STRU --change-batch file:///tmp/record.json &>/dev/null
+
+
+echo DNS Record Created
+
+
+
+
+
+
